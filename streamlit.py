aba0bc82a2adc8fa795c1fa21d397493b02c5e16
IDRTeam <129487199+IDRTeam@users.noreply.github.com>
2023-03-31 15:39:31 +0530

    Update streamlit.py
    api changes

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,9 @@
 
 import streamlit
-
+import requests
 streamlit.title("My parents new healthy dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega3 contains meal")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
