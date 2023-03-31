877ebbd9ccd04175fe76f8bd73d29578874891fc
IDRTeam <129487199+IDRTeam@users.noreply.github.com>
2023-03-31 12:20:26 +0530

    Update streamlit.py
    type mistake

--- streamlit.py
+++ streamlit.py
@@ -3,4 +3,4 @@ import streamlit
 
 streamlit.title("My parents new healthy dinner")
 streamlit.header("Breakfast Menu")
-streamlit.test("Omega3 contains meal")
+streamlit.text("Omega3 contains meal")
