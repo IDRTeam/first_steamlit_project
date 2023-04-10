20a82b9e2a91dc159f48c5996fd3ae3411022413
IDRTeam <129487199+IDRTeam@users.noreply.github.com>
2023-04-10 14:21:11 +0530

    Update streamlit.py
    delete snowpark context

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
 import pandas as pd
-from snowflake.snowpark.context
+
 
 st.set_page_config(
      page_title="Environment Data Atlas",
