5b45b4a167cd38ca16a001bb27849b0c5f034949
IDRTeam <129487199+IDRTeam@users.noreply.github.com>
2023-04-12 19:26:42 +0530

    Update streamlit.py
    changes

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,8 @@
+# Snowpark
 from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
 import pandas as pd
-
 
 st.set_page_config(
      page_title="Environment Data Atlas",
@@ -18,39 +18,29 @@ st.set_page_config(
 # Create Session object
 def create_session_object():
     connection_parameters = {
-      "account"   : "ZX16444",
+      "account"   : "LJ22006.ap-south-1",
       "user"      : "malakamboj",
       "password"  : "Rupashi@20",
       "role"      : "ACCOUNTADMIN",
       "warehouse" : "COMPUTE_WH",
       "database"  : "TESTBYMALA",
-      "schema"    : "Public"
+      "schema"    : "PUBLIC"
     }
     session = Session.builder.configs(connection_parameters).create()
     return session
 
 # Add header and a subheader
-st.header("IDR : First Demo")
-st.subheader("Central Data FOR IDR")
+st.header("IDR : POC ")
+st.subheader("Powered by Accenture@2023 | Made with Streamlit")
   
 # Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
-def load_data(session):
-  with st.form("update_report"):
-    comment_txt = st.text_area('Report comment:')
-    comment_dt = st.date_input('Date of report:')
-    sub_comment = st.form_submit_button('Submit')
-
-# STEP 4 : WRITE THAT TO A TABLE IN SNOWFLAKE
-if sub_comment:
-        session.sql(f"""INSERT INTO DB.SCHEMA.TABLE (DATE, USER, COMMENT) 
-    VALUES ('{comment_dt}', CURRENT_USER(), '{comment_txt}')""").collect()
-        st.success('Success!', icon="✅")
-
-# STEP 5 : PRESENT THE TABLE IN THE APP
-q_comments = f"""SELECT * FROM DB.SCHEMA.TABLE ORDER BY 1 desc LIMIT 10"""
-df_comments = session.sql(q_comments).to_pandas()
-st.dataframe(df_comments, use_container_width=True)
-
+def load_data(session):    # CO2 Emissions by Country
+   df = session.table("PERSON")
+   edited = st.experimental_data_editor(df, use_container_width=True, num_rows="dynamic")
+   session.write_pandas(edited, "ESG_SCORES_DEMO", overwrite=True)
+   
+ 
 if __name__ == "__main__":
     session = create_session_object()
     load_data(session)
+  
